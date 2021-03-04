55f45ee840693fcd396766cdd0a64690d4f07125
mmmfang <mae.fang@gmail.com>
2021-03-04 04:16:30 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -15,6 +15,6 @@ function addElementToEndOfArray(array, el){
   return newArray = [...array, el];
 }
 function destructivelyAddElementToEndOfArray(array, el){
-    var newArray = [];
-    return newArray = array.push(el);
+
+    return array.push(el);
 }
