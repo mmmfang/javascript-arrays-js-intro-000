90c246d2d3ffbfb8dcdd81fe272d114e92dccc97
mmmfang <mae.fang@gmail.com>
2021-03-04 04:26:59 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -34,3 +34,8 @@ function destructivelyRemoveElementFromEndOfArray(array){
   array.pop();
   return array;
 }
+
+function removeElementFromEndOfArray(array){
+  var newArray = array.slice(-1);
+  return newArray;
+}
