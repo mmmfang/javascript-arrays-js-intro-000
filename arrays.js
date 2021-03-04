8e29032bfdac4968930b1dd3eabf89051156c664
mmmfang <mae.fang@gmail.com>
2021-03-04 02:36:33 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,13 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+//let chocolateBars = new Array('snickers', 'hundred grand', 'kitkat', 'skittles');
+function addElementToBeginningOfArray (array,el){
+  //let array = [];
+  let newArray = [el, ...array];
+  return newArray;
+};
+
+function destructivelyAddElementToBeginningOfArray(arr, el){
+  let arr = [];
+  arr.unshift(el);
+  return arr;
+}
