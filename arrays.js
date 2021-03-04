d054c143a3de86ffa2d25637ff575308c73c75da
mmmfang <mae.fang@gmail.com>
2021-03-04 04:24:24 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -27,6 +27,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  var  newArray = array.slice(0);
+  var newArray = array.slice(1);
   return newArray;
 }
